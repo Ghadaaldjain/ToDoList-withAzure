7494667d908b2ae411a1bcfd21b4f9c664ef3bcf
ghadaaldjain <ghadaaldjain@gmail.com>
2021-01-22 17:39:10 +0300

    Add DB Context

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Item> Items { get; set; }
+    }
+}
