6b5e76b9e6e52b5e6fce97f5fb7728ca3541e1bc
ghadaaldjain <ghadaaldjain@gmail.com>
2021-01-22 17:39:32 +0300

    Add Item Model

---
+++ Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ToDoList.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
